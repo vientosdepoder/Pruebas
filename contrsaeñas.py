b32c5ea2ac3e3fa3542e563df752bee6606d12fe
Aurea1357 <sofigomi.sg@gmail.com>
2020-07-21 19:04:50 -0300

    segundo commit

--- contrsaeñas.py
+++ contrsaeñas.py
@@ -4,3 +4,5 @@ longitud = 10
 # La longitud que queremos
 cadena_aleatoria = ''.join(choice(caracteres) for caracter in range(longitud))
 print("La cadena es: ", cadena_aleatoria)
+#no.
+print("loquequiera")
